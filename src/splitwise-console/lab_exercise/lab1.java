fb134b098b372d05501f4bbba0d6df09118038b2
Dharshini sivasubramaniam <231696172+dharshinisivasubramaniam@users.noreply.github.com>
2026-08-18 20:05:16 +0530

    lab1

---
+++ src/splitwise-console/lab_exercise/lab1.java
@@ -0,0 +1,17 @@
+package lab_exercise;
+class lab1{
+    public static void main(String[] args) {
+        int count=10;
+        long long_num=1005l;//1005L
+        float frac=10.5f;//10.5F
+        double salary=10.54;
+        boolean value=true;
+        char letter='D';
+        System.out.println(count);
+        System.out.println(long_num);
+        System.out.println(frac);
+        System.out.println(salary);
+        System.out.println(value);
+        System.out.println(letter);
+    }
+}
