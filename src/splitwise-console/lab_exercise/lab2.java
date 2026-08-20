6de3a7792c8c0a1e9fe2a50cfe52b8ce421fe996
Dharshini sivasubramaniam <231696172+dharshinisivasubramaniam@users.noreply.github.com>
2026-08-20 20:00:50 +0530

    lab2 C

--- src/splitwise-console/lab_exercise/lab2.java
+++ src/splitwise-console/lab_exercise/lab2.java
@@ -1,9 +1,12 @@
 package lab_exercise;
 
+import java.util.Scanner;
 class lab2
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
+
         //Arithmetic
         System.out.println("---Arithmetic---");
         System.out.println("Integer/Integer: "+17/5); // int/int -> int
@@ -22,6 +25,34 @@ class lab2
         System.out.println(choice==2); // check for equal
         System.out.println(choice!=3); // check for not equal
         System.out.println(choice>1 && choice<=3); // boolean operator to combine two relational expression
+
+        System.out.println();
+
+        //if / else if
+        System.out.println("if / else if");
+        System.out.println("---Menu---"); // displaying menu
+        System.out.println("1. Add");
+        System.out.println("2. View");
+        System.out.println("3. Exit");
+        System.out.print("Enter choice:");
+        int choice1=sc.nextInt(); //get input from user
+        if(choice1==1) // enter when input is 1
+        {
+            System.out.println("Add");
+        }
+        else if (choice1==2)  // enter when input is 2
+        {
+            System.out.println("View");
+        }
+        else if (choice1==3)  // enter when input is 3
+        {
+            System.out.println("Exit");
+        }
+        else // enter when no input matches
+        {
+            System.out.println("Invalid choice");
+        }
+
 
     }
 }
